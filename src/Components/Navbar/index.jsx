8b893d255e1ddf2d03d4af6f8a585e80fa4b5ed5
Carlos Anderson <cahanderson@gmail.com>
2022-07-15 16:47:46 -0300

    Correcao de conteudo

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+export function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-md">
+        <a class="navbar-brand" href="/">
+          Meu Blog
+        </a>
+      </div>
+    </nav>
+  );
+}
